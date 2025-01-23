05697d214b55030ee51e589a532543ae2464fbd6
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-01-23 22:25:57 +0300

    add settler selection

---
+++ Assets/Scripts/Settlers/SettlersSelectionManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class SettlersSelectionManager : MonoBehaviour
+{
+    public static SettlersSelectionManager Instance;
+        
+    [SerializeField]private CommandsPanel _commandsPanel;
+    [SerializeField]private TacticalCommandPanel _tacticalCommandPanel;
+    [SerializeField]private ChangeModeToggle _changeModeToggle;
+
+    public Chamomile SelectedSettler { get; private set; }
+        
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (EventSystem.current?.IsPointerOverGameObject() == true) return;
+            if (!hit) 
+            {
+                TryUnselectSettler();
+                return;
+            }
+            if (hit.transform.TryGetComponent(out Chamomile settler))
+            {
+                TryUnselectSettler();
+                SelectSettler(settler);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                TryUnselectSettler();
+            }
+        }
+    }
+
+    private void SelectSettler(Chamomile settler)
+    {
+        if (SelectedSettler == null)
+        {
+            SelectedSettler = settler;
+            _changeModeToggle.gameObject.SetActive(true);
+        }
+    }
+
+    private void TryUnselectSettler()
+    {
+        if (SelectedSettler != null && _tacticalCommandPanel.SelectedTacticalCommand == TacticalCommand.None)
+        {
+            _changeModeToggle.gameObject.SetActive(false);
+            _changeModeToggle.GetComponent<Toggle>().isOn = false;
+            SelectedSettler.ChangeMode(Mode.Planning);
+            SelectedSettler = null;
+        }
+    }
+}
---
+++ Assets/Scripts/WorldObjects/Creatures/SelectableSettler.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SelectableSettler : MonoBehaviour
+{
+    [SerializeField] Chamomile _settler;
+    
+}
