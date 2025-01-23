f0a997a0e9b6c14cd226ca3a2d608ea6972f3e6e
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-01-24 01:59:52 +0300

    add tactical command interact check

--- Assets/Scripts/GridHelpers/ExactInteractionChecker.cs
+++ Assets/Scripts/GridHelpers/ExactInteractionChecker.cs
@@ -1,9 +1,14 @@
 using System.Linq;
 using UnityEngine;
+using WorldObjects;
 
 public static class ExactInteractionChecker {
     public static bool CanInteract(Vector2Int from, Interactable to) {
-        return from.x == to.GetInteractableSell.x && from.y == to.GetInteractableSell.y;
+        return from.x == to.GetInteractableCell.x && from.y == to.GetInteractableCell.y;
+    }
+    
+    public static bool CanInteract(Vector2Int from, TacticalInteractable to) {
+        return from.x == to.GetInteractableCell.x && from.y == to.GetInteractableCell.y;
     }
 
     public static Vector2Int? NextStepOnPath(Vector2Int from, Vector2Int to) {
