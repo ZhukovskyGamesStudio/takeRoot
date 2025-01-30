e5051c3a1456489282ebc2ca74be22a665c63c50
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-01-30 20:47:06 +0300

    update resource view

--- Assets/Scripts/WorldObjects/ResourceView.cs
+++ Assets/Scripts/WorldObjects/ResourceView.cs
@@ -45,14 +45,17 @@ public class ResourceView : ECSEntity {
     public void SetAmount(int amount) {
         _amountText.text = amount.ToString();
         Amount = amount;
-        ResorceData.Amount = amount;
+        ResourceData.Amount = amount;
     }
 
     private void OnCommandPerformed(Command obj) {
         if (obj == Command.Transport) {
             IsBeingCarried = true;
+            _interactable.CanSelect = false;
             _interactable.CommandToExecute.CommandType = Command.Store;
-            
+            //var storage = ResourceManager.Instance.FindEmptyStorageForResorce(ResourceData);
+            //Interactable interactableStorage = storage.GetEcsComponent<Interactable>();
+            //_interactable.CommandToExecute.Additional = interactableStorage;
             CommandsManager.Instance.AddSubsequentCommand(_interactable.CommandToExecute);
             transform.SetParent(_interactable.CommandToExecute.Settler.transform);
             if (_interactable.CommandToExecute.PlannedCommandView != null) {
@@ -63,17 +66,14 @@ public class ResourceView : ECSEntity {
         if (obj == Command.Store) {
             _interactable.CommandToExecute.Additional.GetComponent<Storagable>().AddResource(ResourceData);
             _interactable.CancelCommand();
-            _interactable.OnDestroyed();
-            Destroy(gameObject);
+            SetAmount(ResourceData.Amount);
+            if(ResourceData.Amount == 0)
+                _interactable.OnDestroyed();
         }
     }
 
     private void OnCommandCanceled(Command type) {
-        if (IsBeingCarried) {
-            IsBeingCarried = false;
-            transform.SetParent(ResourceManager.Instance.transform);
-            _interactable.CommandToExecute.CommandType = Command.Transport;
-        }
+            DropOnGround();
     }
 
     public void DropOnGround() {
@@ -82,8 +82,9 @@ public class ResourceView : ECSEntity {
         }
 
         IsBeingCarried = false;
-        transform.SetParent(ResourceManager.Instance.transform);
-        _interactable.CommandToExecute.CommandType = Command.Transport;
+        _interactable.CanSelect = true;
+        _interactable.Gridable.PositionChanged();
+        transform.SetParent(ResourceManager.Instance.ResourcesHolder);
     }
 }
 
