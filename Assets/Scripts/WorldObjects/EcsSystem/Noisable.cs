bfcecbb307bdfe99ac70706304f66b5092b51cea
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-02-21 22:14:08 +0300

    add Noisable and Listener component

---
+++ Assets/Scripts/WorldObjects/EcsSystem/Noisable.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using WorldObjects;
+
+
+public class Noisable : ECSComponent
+{
+    [SerializeField]private int _noiseRadius;
+    private Gridable _gridable;
+        
+    public void MakeNoise()
+    {
+        var noisePosition = VectorUtils.ToVector2Int(_gridable.GetCenterOnGrid);
+        var listeners = FindObjectsByType<Listener>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+        
+        HashSet<Vector2Int> noisePoints = new HashSet<Vector2Int>();
+            
+        var topLeft = new Vector2Int(noisePosition.x - _noiseRadius, noisePosition.y + _noiseRadius);
+        var botRight = new Vector2Int(noisePosition.x + _noiseRadius, noisePosition.y - _noiseRadius);
+            
+        for (int x = topLeft.x; x < botRight.x; x++)
+        for (int y = topLeft.y; y > botRight.y; y--)
+        {
+            noisePoints.Add(new Vector2Int(x, y));
+        }
+        foreach (Listener listener in listeners)
+        {
+            foreach (Vector2Int occupiedPosition in listener.Gridable.GetOccupiedPositions())
+                if (noisePoints.Contains(occupiedPosition))
+                    listener.HasHeard?.Invoke(noisePosition);
+        }
+    }
+        
+    public override int GetDependancyPriority()
+    {
+        return 0;
+    }
+
+    public override void Init(ECSEntity entity)
+    {
+        _gridable = entity.GetEcsComponent<Gridable>();
+    }
+}
