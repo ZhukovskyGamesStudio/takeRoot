2e08a461a9caf9c0a4664f2d6e6b3aff4f242e95
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-06-25 18:41:53 +0300

    add write resource view to resources global data

--- Assets/Scripts/Infrastructure/Factory/GameFactory.cs
+++ Assets/Scripts/Infrastructure/Factory/GameFactory.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 
 public class GameFactory : IGameFactory
 {
-    private readonly IAssetProvider _assetProvider;
+	private readonly IAssetProvider _assetProvider;
+	private readonly IDataProvider _dataProvider;
+	
+	
+	public GameFactory(IAssetProvider assetProvider, IDataProvider dataProvider) {
+		_assetProvider = assetProvider;
+		_dataProvider = dataProvider;
+	}
+	
+	public GameObject CreateSettler(string settlerTypeId, Vector3 at) {
+		return _assetProvider.Instantiate($"{AssetPath.SettlersPath}{settlerTypeId}", at);
+	}
 
-    public GameFactory(IAssetProvider assetProvider)
-    {
-        _assetProvider = assetProvider;
-    }
-    public GameObject CreateSettler(string settlerTypeId, Vector3 at)
-    {
-       return _assetProvider.Instantiate($"{AssetPath.SettlersPath}{settlerTypeId}", at);
-    }
+	public GameObject CreateResource(string resourceTypeId, Vector3 at, int amount) {
+		var resource = _assetProvider.Instantiate($"{AssetPath.ResourcesPath}{resourceTypeId}", at);
+        
+		var resourceView = resource.GetComponent<ResourceView>();
+		resourceView.SetAmount(amount);
+		
+		_dataProvider.WorldResourcesData.ResourcesOnScene[new Vector2Int((int)resource.transform.position.x, (int)resource.transform.position.y)] = resourceView;
+		return resource;
+	}
 
-    public GameObject CreateResource(string resourceTypeId, Vector3 at, int amount)
-    {
-        return _assetProvider.Instantiate($"{AssetPath.ResourcesPath}{resourceTypeId}", at);
-    }
 }
