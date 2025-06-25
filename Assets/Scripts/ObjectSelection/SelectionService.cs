920867cd23e219b65ed9c81c673db2a4a9776e88
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-06-25 20:44:09 +0300

    add object selection

---
+++ Assets/Scripts/ObjectSelection/SelectionService.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+public class SelectionService : ISelectionService, IUpdatable
+{
+	private readonly IInputService _input;
+	private readonly IPhysicsService _physics;
+	public ISelectableObj Selected { get; private set; }
+
+	public SelectionService(IInputService inputService, IPhysicsService physics, IUpdateService updateService) {
+		_input = inputService;
+		_physics = physics;
+		updateService.Register(this);
+	}
+
+	public void Update() {
+		if (_input.GetMouseButtonDown(MouseButton.Left)) {
+			ISelectableObj selectable = _physics.Raycast<ISelectableObj>(_input.GetWorldMousePosition(), Vector2.zero);
+			if (selectable != null && selectable != Selected) {
+				SetSelected(selectable);
+				Debug.Log("Selecterd");
+			}
+			else {
+				Debug.Log("Unselected!");
+				Selected = null;
+			}
+
+		}
+	}
+	
+	private void SetSelected(ISelectableObj selectable) {
+		Selected = selectable;
+	}
+}
