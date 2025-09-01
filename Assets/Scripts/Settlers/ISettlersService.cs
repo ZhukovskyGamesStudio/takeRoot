5c89f15abc8c2f6233c9e6ac2cb7292cfe2153d3
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-09-02 02:59:31 +0300

    add zombie find nearby settler

--- Assets/Scripts/Settlers/ISettlersService.cs
+++ Assets/Scripts/Settlers/ISettlersService.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public interface ISettlersService : IService {
+    public AI.Settler GetSettlerInArea(Rect area);
     public List<AI.Settler> MySettlers(Race myRace);
 }
