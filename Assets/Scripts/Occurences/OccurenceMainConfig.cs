144f070a99915a693559f8f5a9feff76a33b6221
GrafEnters <graf.enters@gmail.com>
2025-09-01 03:27:51 +0200

    zombie spawn occurence

---
+++ Assets/Scripts/Occurences/OccurenceMainConfig.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OccurenceMainConfig", menuName = "Scriptable Objects/OccurenceMainConfig", order = 0)]
+public class OccurenceMainConfig : ScriptableObject {
+    public float DifficultyPointsPerMinute;
+}
--- Assets/Scripts/Occurences/OccurenceService.cs
+++ Assets/Scripts/Occurences/OccurenceService.cs
@@ -1,16 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
 public class OccurenceService : IOccurenceService, IUpdatable {
     private readonly IUpdateService _updateService;
+    private List<OccurenceConfig> _occurencesConfigs;
+    private OccurenceMainConfig _mainConfig;
+    private float _difficltyPoints = 0;
 
     public OccurenceService(IConfigsProvider configsProvider, IUpdateService updateService) {
         _updateService = updateService;
+        _mainConfig = configsProvider.OccurenceMainConfig;
+        _occurencesConfigs = configsProvider.OccurenceConfigs;
+
         _updateService.Register(this);
     }
 
     public void Update() {
-        
+        _difficltyPoints += _mainConfig.DifficultyPointsPerMinute / 60 * Time.deltaTime;
+        TrySpawnOccurence();
+    }
+    
+    private void TrySpawnOccurence() {
+        foreach (var occurenceConfig in _occurencesConfigs) {
+            if (_difficltyPoints >= occurenceConfig.DifficultyCost) {
+                SpawnOccurence(occurenceConfig);
+                _difficltyPoints -= occurenceConfig.DifficultyCost;
+                break;
+            }
+        }
+    }
+
+    private void SpawnOccurence(OccurenceConfig occurenceConfig) {
+        Object.Instantiate(occurenceConfig.PrefabToSpawn);
+        OnOccurenceSpawn?.Invoke(occurenceConfig);
     }
 
     public void Dispose() {
         _updateService.Unregister(this);
     }
+
+    public Action<OccurenceConfig> OnOccurenceSpawn { get; set; }
 }
