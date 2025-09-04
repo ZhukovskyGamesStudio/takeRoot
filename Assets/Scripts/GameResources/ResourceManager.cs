f21636fe6363d240e379be8ce5d6bc2a5d30ecee
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-09-04 04:33:18 +0300

    add resource random pick

--- Assets/Scripts/GameResources/ResourceManager.cs
+++ Assets/Scripts/GameResources/ResourceManager.cs
@@ -4,96 +4,113 @@ using System.Linq;
 using GameResources;
 using UnityEngine;
 using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 public class ResourcesManager : IResourceManager {
-    private readonly ResourcesConfig _config;
-    private readonly ResourcesTable _config2;
-    private readonly IGridService _grid;
-    private List<Resource> ResourcesPrefabs ;
-    private Dictionary<Vector3, Resource> ExistingResourcesOnGround = new();
+	private readonly ResourcesConfig _config;
+	private readonly ResourcesTable _config2;
+	private readonly IGridService _grid;
+	private List<Resource> ResourcesPrefabs ;
+	private Dictionary<Vector3, Resource> ExistingResourcesOnGround = new();
 
-    public ResourcesManager(IConfigsProvider configService, IGridService grid) {
-        _config = configService.ResourcesConfig;
-        _config2 = configService.ResourcesTable;
-        _grid = grid;
-        ResourcesPrefabs = _config.ResourcesPrefabs;
-    }
+	public ResourcesManager(IConfigsProvider configService, IGridService grid) {
+		_config = configService.ResourcesConfig;
+		_config2 = configService.ResourcesTable;
+		_grid = grid;
+		ResourcesPrefabs = _config.ResourcesPrefabs;
+	}
 
-    public void SpawnResource(Vector3 at, ResourceType type, int amount) {
-        Resource prefab = GetPrefabByType(type);
-        if (prefab == null) {
-            Debug.LogError($"No resource prefab found for {type}");
-            return;
-        }
+	public void SpawnResource(Vector3 at, ResourceType type, int amount) {
+		Resource prefab = GetPrefabByType(type);
+		if (prefab == null) {
+			Debug.LogError($"No resource prefab found for {type}");
+			return;
+		}
 
-        Vector3? spawnPos = PickSpawnPos(at);
-        if (spawnPos == null) {
-            Debug.LogError($"No pos found found for {type}");
-            return;
-        }
-        spawnPos = new Vector3(Mathf.Ceil(spawnPos.Value.x), Mathf.Ceil(spawnPos.Value.y), spawnPos.Value.z);
-        Resource r = Object.Instantiate(prefab, spawnPos.Value, Quaternion.identity);
-        ExistingResourcesOnGround.Add(spawnPos.Value, r);
-        r.Init(amount);
-    }
+		Vector3? spawnPos = PickSpawnPos(at);
+		if (spawnPos == null) {
+			Debug.LogError($"No pos found found for {type}");
+			return;
+		}
+		spawnPos = new Vector3(Mathf.Ceil(spawnPos.Value.x), Mathf.Ceil(spawnPos.Value.y), spawnPos.Value.z);
+		Resource r = Object.Instantiate(prefab, spawnPos.Value, Quaternion.identity);
+		ExistingResourcesOnGround.Add(spawnPos.Value, r);
+		r.Init(amount);
+	}
 
-    public void DestroyResource(Vector3 at) {
-        var resource = ExistingResourcesOnGround[at];
-        ExistingResourcesOnGround.Remove(at);
-        Object.Destroy(resource.gameObject);
-    }
+	public void DestroyResource(Vector3 at) {
+		var resource = ExistingResourcesOnGround[at];
+		ExistingResourcesOnGround.Remove(at);
+		Object.Destroy(resource.gameObject);
+	}
 
-    public Resource FindResourceOnGround(ResourceType type) {
-        foreach (Resource resource in ExistingResourcesOnGround.Values) {
-            if (resource.Type == type && resource.Amount - resource.Reserved > 0) {
-                return resource;
-            }
-        }
+	public Resource FindResourceOnGround(ResourceType type) {
+		foreach (Resource resource in ExistingResourcesOnGround.Values) {
+			if (resource.Type == type && resource.Amount - resource.Reserved > 0) {
+				return resource;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    //TODO optimize this
-    public Dictionary<ResourceType, int> TotalResources() {
-        Dictionary<ResourceType, int> totalResources = new();
-        foreach (KeyValuePair<Vector3, Resource> resource in ExistingResourcesOnGround) {
-            if (!totalResources.TryAdd(resource.Value.Type, resource.Value.Amount)) {
-                totalResources[resource.Value.Type] += resource.Value.Amount;
-            }
-        }
+	//TODO optimize this
+	public Dictionary<ResourceType, int> TotalResources() {
+		Dictionary<ResourceType, int> totalResources = new();
+		foreach (KeyValuePair<Vector3, Resource> resource in ExistingResourcesOnGround) {
+			if (!totalResources.TryAdd(resource.Value.Type, resource.Value.Amount)) {
+				totalResources[resource.Value.Type] += resource.Value.Amount;
+			}
+		}
 
-        return totalResources;
-    }
+		return totalResources;
+	}
 
-    public Sprite GetResourceSpriteNoShadow(ResourceType type) => _config2.ResourceIconsDictionary[type];
+	public Sprite GetResourceSpriteNoShadow(ResourceType type) => _config2.ResourceIconsDictionary[type];
 
-    private Vector3? PickSpawnPos(Vector3 at) {
-        List<Vector3> positions = GetAroundPos(at);
-        for (int i = 0; i < positions.Count; i++) {
-            var pos = new Vector3(Mathf.Ceil(positions[i].x), Mathf.Ceil(positions[i].y), positions[i].z);
-            if (!ExistingResourcesOnGround.ContainsKey(pos) && !_grid.IsOccupiedPos(pos)) {
-                return positions[i];
-            }
-        }
+	private Vector3? PickSpawnPos(Vector3 at) {
+		List<Vector3> positions = GetAroundPos(at);
+		for (int i = 0; i < positions.Count; i++) {
+			var pos = new Vector3(Mathf.Ceil(positions[i].x), Mathf.Ceil(positions[i].y), positions[i].z);
+			if (!ExistingResourcesOnGround.ContainsKey(pos) && !_grid.IsOccupiedPos(pos)) {
+				return positions[i];
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private List<Vector3> GetAroundPos(Vector3 origin) {
-        return new List<Vector3> {
-            origin,
-            origin + new Vector3(1, 0),
-            origin + new Vector3(1, -1),
-            origin + new Vector3(0, -1),
-            origin + new Vector3(-1, -1),
-            origin + new Vector3(-1, 0),
-            origin + new Vector3(-1, 1),
-            origin + new Vector3(0, 1),
-            origin + new Vector3(1, 1)
-        };
-    }
+	private List<Vector3> GetAroundPos(Vector3 origin) {
+		return new List<Vector3> {
+			origin,
+			origin + new Vector3(1, 0),
+			origin + new Vector3(1, -1),
+			origin + new Vector3(0, -1),
+			origin + new Vector3(-1, -1),
+			origin + new Vector3(-1, 0),
+			origin + new Vector3(-1, 1),
+			origin + new Vector3(0, 1),
+			origin + new Vector3(1, 1)
+		};
+	}
 
-    private Resource GetPrefabByType(ResourceType type) {
-        return ResourcesPrefabs.FirstOrDefault(r => r.Type == type);
-    }
+	private Resource GetPrefabByType(ResourceType type) {
+		return ResourcesPrefabs.FirstOrDefault(r => r.Type == type);
+	}
+
+	public ResourceType GetRandomResourceType() {
+		var total = _config.TotalWeight;
+		var point = 0;
+		var n = Random.Range(0, total);
+		foreach (var kvp in _config.ResourceWeights) {
+			var type = kvp.Key;
+			var weight = kvp.Value;
+			point += weight;
+			if (n < point) {
+				return type;
+			}
+		}
+		return ResourceType.None;
+	}
+    
 }
--- Assets/Scripts/GameResources/ResourcesConfig.cs
+++ Assets/Scripts/GameResources/ResourcesConfig.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace GameResources {
     [CreateAssetMenu(fileName = "ResourcesConfig", menuName = "GameResources/ResourcesConfig")]
     public class ResourcesConfig : ScriptableObject {
         public List<Resource> ResourcesPrefabs;
+        public AYellowpaper.SerializedCollections.SerializedDictionary<ResourceType, int> ResourceWeights;
+        public int TotalWeight => ResourceWeights.Select(r => r.Value).Sum();
+        
     }
 }
