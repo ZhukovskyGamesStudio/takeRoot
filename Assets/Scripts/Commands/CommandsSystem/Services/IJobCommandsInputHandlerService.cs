668cf326565e829de3d33b06c73507e30f4d37a6
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-09-06 06:27:24 +0300

    fix command toggles mouse pick

--- Assets/Scripts/Commands/CommandsSystem/Services/IJobCommandsInputHandlerService.cs
+++ Assets/Scripts/Commands/CommandsSystem/Services/IJobCommandsInputHandlerService.cs
@@ -1,6 +1,8 @@
+using System;
 using UniRx;
 
 public interface IJobCommandsInputHandlerService : IService {
+    public Action<JobType> OnJobChanged { get; set; }
     public ReactiveProperty<JobType> PendingCommand { get; set; }
     public bool IsEnabled { get; set; }
 }
